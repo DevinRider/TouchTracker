ce4f80c8b66e1863d837e856706ec2d5060bbbd2
Devin Rider <devinrrider@gmail.com>
2014-06-12 09:02:22 -0700

    Initial Commit

---
+++ TouchTracker/DEVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEVAppDelegate.h
+//  TouchTracker
+//
+//  Created by Devin on 6/12/14.
+//  Copyright (c) 2014 Devin Rider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by Devin on 6/12/14.
+//  Copyright (c) 2014 Devin Rider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DEVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DEVAppDelegate class]));
+    }
+}
